cc87b263fe9eb67ba2b6e602a6f7b9b2b6500294
Abdelrahime7 <abdououni67@gmail.com>
2025-09-25 09:45:07 +0100

    - feat(users): add DTOs for user creation and response shaping
    - Introduced CreateUserRequest ,UpdateUserRequest and UserResponse models.
    - Enables clean separation of input/output logic in user workflows.

---
+++ src/Application/Users/DTOs/UpdateUserRequest.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Domain.Enums;
+
+namespace Application.Users.DTOs
+{
+    public record UpdateUserRequest
+    {
+        public int Id { get; set; }
+        public required string FullName { get; set; }
+        public required string Email { get; set; }
+        public required string Phone { get; set; }
+        public required string PasswordHash { get; set; }
+        public string? CcpNumber { get; set; }
+        public UserStatus Status { get; set; }
+        public DateTime? LastLoginAt { get; set; }
+        public int? RoleId { get; set; }
+    }
+}
