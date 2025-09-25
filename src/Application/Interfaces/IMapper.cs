bab81a9500f56c51fc0fa817a647a7a20f1bdad6
Abdelrahime7 <abdououni67@gmail.com>
2025-09-25 15:47:03 +0100

    Added support for update DTO mapping 🎯
    • Extended IMapper with ToUpdateEntity<TUpdateDto, TEntity> for clean update workflows
    • Implemented mapping logic in UserMapper for UpdateUserRequest → User
    • Makes entity updates more consistent and reusable across the app

--- src/Application/Interfaces/IMapper.cs
+++ src/Application/Interfaces/IMapper.cs
@@ -5,6 +5,7 @@ namespace Application.Interfaces
    public interface IEntityMapper<TEntity, TCreateDto, TUpdateDto, TResponseDto>
 {
     TEntity ToEntity(TCreateDto dto);
+    TEntity ToUpdateEntity(TUpdateDto dto);
     TResponseDto ToResponse(TEntity entity);
 }
 }
--- src/Application/Users/Mapper/UserMapper.cs
+++ src/Application/Users/Mapper/UserMapper.cs
@@ -1,14 +1,14 @@
 ﻿using Application.Interfaces;
 using Application.Users.DTOs;
 using Domain.Entities;
-using Domain.Enums;
+
 using Microsoft.AspNetCore.Identity;
 
 namespace Application.Users.Mapper
 {
     public class UserMapper : IEntityMapper<User, CreateUserRequest, UpdateUserRequest, UserResponse>
     {
-        public User ToEntity (CreateUserRequest dto)
+        public User ToEntity(CreateUserRequest dto)
         {
             var PasswordHasher = new PasswordHasher<object>();
 
@@ -17,12 +17,14 @@ namespace Application.Users.Mapper
                 FullName = dto.FullName,
                 Email = dto.Email,
                 Phone = dto.Phone,
-                PasswordHash = PasswordHasher.HashPassword(null!,dto.PasswordHash),
+                PasswordHash = PasswordHasher.HashPassword(null!, dto.PasswordHash),
                 CcpNumber = dto.CcpNumber,
                 Status = dto.Status,
                 RoleId = dto.RoleId,
             };
         }
+
+       
 
         public UserResponse ToResponse(User entity)
         {
@@ -35,6 +37,22 @@ namespace Application.Users.Mapper
                 LastLoginAt = entity.LastLoginAt,
                 RoleId = entity.RoleId,
                 Status= entity.Status,
+            };
+        }
+
+        public User ToUpdateEntity(UpdateUserRequest dto)
+        {
+            return new User
+            {
+                Id = dto.Id,
+                FullName = dto.FullName,
+                PasswordHash = dto.PasswordHash,
+                Email = dto.Email,
+                Phone = dto.Phone,
+                CcpNumber = dto.CcpNumber,
+                LastLoginAt = dto.LastLoginAt,
+                RoleId = dto.RoleId,
+                Status = dto.Status
             };
         }
 
