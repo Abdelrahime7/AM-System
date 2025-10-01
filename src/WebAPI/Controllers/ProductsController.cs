85b6654d8b498498bb0c0da5ba322fc8d6dbf2e9
Hocine-Bec <bechebil.houcin@gmail.com>
2025-10-01 10:50:14 +0100

    feat(api): Add API controller for product management
    - Introduce `ProductsController` to expose product CRUD operations via the REST API.
    - Implement endpoints for creating, reading, updating, and deleting products.
      - `GET /api/products`: Retrieve all products.
      - `GET /api/products/{id}`: Retrieve a product by ID.
      - `POST /api/products`: Create a new product.
      - `PUT /api/products`: Update an existing product.
      - `DELETE /api/products`: Delete a product.
    - The controller injects and utilizes `IProductQueries` and `IProductCommands` to process requests and returns appropriate HTTP responses.

---
+++ src/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,123 @@
+using System.Security.Claims;
+using Application.Customers.DTOs;
+using Application.Interfaces.ProductInterfaces;
+using Application.Products.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("api/products")]
+public class ProductsController(IProductQueries productQueries, IProductCommands productCommands)
+    : ControllerBase
+{
+    private readonly IProductCommands _productCommands = productCommands;
+    private readonly IProductQueries _productQueries = productQueries;
+    
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<ActionResult<IEnumerable<ProductResponse>>> GetAll()
+    {
+        var result = await _productQueries.GetAllAsync();
+
+        if (result.IsSuccess)
+            return Ok(result.Value);
+
+        return NoContent();
+    }
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<ProductResponse>> GetById(int id)
+    {
+        if (id < 1)
+            return BadRequest($"Invalid id = {id}");
+
+        var result = await _productQueries.GetByIdAsync(id);
+        if (result.IsSuccess)
+            return Ok(result.Value);
+
+        return NotFound();
+    }
+
+    [HttpGet("by-name")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<ProductResponse>> GetByName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+            return BadRequest($"Invalid name = {name}");
+
+        var result = await _productQueries.GetByNameAsync(name);
+        if (result.IsSuccess)
+            return Ok(result.Value);
+
+        return NotFound();
+    }
+    
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<int>> Create(CreateProductRequest request)
+    {
+        //Uncomment later when we add JWT
+        // request.CreatedByUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+        var result = await _productCommands.CreateProductAsync(request);
+        if (result.IsSuccess)
+            return CreatedAtAction(nameof(GetById), new { id = result.Value }, request);
+
+        return BadRequest(result.Error);
+    }
+
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<ProductResponse>> Update(UpdateProductRequest request)
+    {
+        if (request.Id <= 0) 
+            return BadRequest("Invalid input. Please check the submitted data");
+
+        //Uncomment later when we add JWT
+        // request.CreatedByUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+        var result = await _productCommands.UpdateProductAsync(request);
+        if (result.IsSuccess)
+            return Ok();
+
+        return NotFound(result.Error);
+    }
+
+
+    [HttpDelete]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<ActionResult<bool>> Delete(int id)
+    {
+        if (id < 1)
+            return NoContent();
+        var result = await _productCommands.DeleteProductAsync(id);
+
+        if (!result.IsSuccess)
+            return BadRequest("Product not deleted");
+            
+        return Ok(result.Value);
+    }
+}
