e9072e84a016a498974309c57c6de9ca5331a496
Abdelrahime7 <abdououni67@gmail.com>
2025-10-05 11:49:25 +0100

    feat(order): implement GetAllOrders query
    - Added logic to GetAllOrders.cs for session-based orders retrieval

--- src/Application/Orders/Features/Queries/GetAllOrders.cs
+++ src/Application/Orders/Features/Queries/GetAllOrders.cs
@@ -1,15 +1,41 @@
 using Application.Common.Models;
+using Application.CustomizedOrders.DTOs;
 using Application.Interfaces.OrderInterfaces;
+using Application.OrderDetails.DTOs;
 using Application.Orders.DTOs;
 using Application.Orders.DTOs.Session;
+using System.Collections.Generic;
 
 namespace Application.Orders.Features.Queries;
 
 public partial class OrderQueries 
 {
-    public Task<Result<IEnumerable<ResponseSession>>> GetAllOrdersAsync()
+    public async Task<Result<IEnumerable<ResponseSession>>> GetAllOrdersAsync()
     {
-        throw new NotImplementedException();
+        try
+        {
+            var Orders = await _repository.GetAllAsync();
+            if (Orders.Count() < 0)
+            {
+                return Result<IEnumerable<ResponseSession>>.Failure("No Orders found");
+            }
+
+            var orderResponses = new List<ResponseSession>();
+            foreach (var order in Orders)
+            {
+
+                var response = await GetOrderByIDAsync(order.Id);
+                orderResponses.Add(response.Value);
+
+            }
+
+            return Result<IEnumerable<ResponseSession>>.Success(orderResponses);
+        }
+        catch(Exception ex) 
+        {
+            return Result<IEnumerable<ResponseSession>>.Failure($"Failed to fetch orders: {ex.Message}");
+        }
+
     }
 
    
