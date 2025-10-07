4840bcf63e90a214b20a9ebb350f9d39695d5967
Hocine-Bec <bechebil.houcin@gmail.com>
2025-10-07 14:27:45 +0100

    feat(infra): Add local file storage service
    - Create IFileStorageService interface for handling file operations.
    - Implement FileStorageService for storing files on the local server in the wwwroot/uploads/product-images directory.
    - The service provides methods to upload, delete, and check for the existence of files, which will be used for managing product images.
    - Register IFileStorageService in DependencyInjection.cs

---
+++ src/Application/Interfaces/Repositories/IFileStorageService.cs
@@ -0,0 +1,9 @@
+namespace Application.Interfaces.Repositories;
+
+public interface IFileStorageService
+{
+    Task<string> UploadFileAsync(Stream fileStream, string fileName,string contentType,
+        CancellationToken cancellationToken = default);
+    Task<bool> DeleteFileAsync(string fileUrl, CancellationToken cancellationToken = default);
+    Task<bool> FileExistsAsync(string fileUrl, CancellationToken cancellationToken = default);
+}
