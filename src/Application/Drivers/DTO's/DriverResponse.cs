77a9815f5bf562a66bf0b74c7b022939b0aeb978
Abdelrahime7 <abdououni67@gmail.com>
2025-10-20 09:55:38 +0100

    feat(driver): introduce Driver entity and application layer support
    - Added \Driver\ entity to Domain layer with core properties
    - Created DTOs for create, update, and read operations
    - Implemented AutoMapper profile for entity-to-DTO mapping

---
+++ src/Application/Drivers/DTO's/DriverResponse.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Drivers.DTO_s
+{
+    public class DriverResponse
+    {
+      
+        public bool IsLocal { get; set; }
+        public bool IsAvailable { get; set; }
+        public int UserID { get; set; }
+      
+
+    }
+}
---
+++ src/Application/Drivers/DTO's/UpdateDriverRequest.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Application.Drivers.DTO_s
+{
+    internal class UpdateDriverRequest
+    {
+        public int Id { get; set; }
+        public bool? IsLocal { get; set; }
+        public bool? IsAvailable { get; set; }
+        public int? UserID { get; set; }
+
+    }
+}
