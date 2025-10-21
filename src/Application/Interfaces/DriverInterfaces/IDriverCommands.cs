8fdeecfad9f238feade63779356d597af1c470f9
Abdelrahime7 <abdououni67@gmail.com>
2025-10-21 09:40:04 +0100

    refact(DriverCommands):use creatDriverSession dto instead of creatdriver , to ensure gathering driver and user  at added operation

--- src/Application/Interfaces/DriverInterfaces/IDriverCommands.cs
+++ src/Application/Interfaces/DriverInterfaces/IDriverCommands.cs
@@ -1,12 +1,13 @@
 ﻿using Application.Common.Models;
 using Application.Drivers.DTO_s;
+using Application.Drivers.DTO_s.session;
 using Domain.Entities;
 
 namespace Application.Interfaces.DriverInterfaces
 {
     public interface IDriverCommands
     {
-        public Task<Result<int>> CreateDriverAsync(CreateDriverRequest request);
+        public Task<Result<int>> CreateDriverAsync(CreatDriverSession request);
         public Task<Result<bool>> DeleteDriverAsnc(int Id);
         public  Task <Result<bool>> UpdateDriverAsnc(UpdateDriverRequest request);
 
