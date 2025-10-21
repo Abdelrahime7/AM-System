ae5a613f0b8dcc0fd8b1165f4614de86dae5f26e
Abdelrahime7 <abdououni67@gmail.com>
2025-10-21 09:42:07 +0100

    refact(driverCommands) : adding user then use it as navigation property to driver

--- src/Application/Drivers/features/Commands/CreateDriverCommand.cs
+++ src/Application/Drivers/features/Commands/CreateDriverCommand.cs
@@ -2,26 +2,34 @@
 
 using Application.Common.Models;
 using Application.Drivers.DTO_s;
+using Application.Drivers.DTO_s.session;
 using Application.Interfaces.Common.Mappers;
 using Application.Interfaces.DriverInterfaces;
 using Application.Interfaces.Repositories;
+using Application.Interfaces.UserInterfaces;
 using Domain.Entities;
 
 namespace Application.Drivers.features.Commands
 {
     partial class DriverCommands (IDriverRepository repository,
+        IUserCommands commands,
          IEntityMapper<Driver,CreateDriverRequest,
              UpdateDriverRequest,DriverResponse> mapper) : IDriverCommands 
     {
         private readonly IDriverRepository _repository = repository;
+        private readonly IUserCommands _userCommands = commands;
         private readonly IEntityMapper<Driver, CreateDriverRequest,
              UpdateDriverRequest, DriverResponse> _mapper= mapper;
 
-        public async Task<Result<int>> CreateDriverAsync(CreateDriverRequest request)
+        public async Task<Result<int>> CreateDriverAsync(CreatDriverSession request)
         {
             try
             {
-                var Driver = _mapper.ToEntity(request);
+               var User=  await _userCommands.CreatUserAsync(request.UserRequest);
+                
+                var Driver = _mapper.ToEntity(request.DriverRequest);
+
+                Driver.User = User.Value;
                  await _repository.AddAsync(Driver);
 
                 return Result<int>.Success(Driver.Id);
