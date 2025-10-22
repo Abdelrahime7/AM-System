2ce34381e0c3f0fd361a6f60ca1b665006b87277
Abdelrahime7 <abdououni67@gmail.com>
2025-10-22 10:47:35 +0100

    feat(Migrayions) add DriverMigration and update database

---
+++ src/Infrastructure/Migrations/20251022084350_AddDriverMigration.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDriverMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_users_roles_role_id",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "ccp_number",
+                table: "users");
+
+            migrationBuilder.RenameColumn(
+                name: "role_id",
+                table: "users",
+                newName: "RoleId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_users_role_id",
+                table: "users",
+                newName: "IX_users_RoleId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Drivers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    IsLocal = table.Column<bool>(type: "boolean", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "boolean", nullable: false),
+                    UserID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drivers", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Drivers_users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drivers_UserID",
+                table: "Drivers",
+                column: "UserID",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_users_roles_RoleId",
+                table: "users",
+                column: "RoleId",
+                principalTable: "roles",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_users_roles_RoleId",
+                table: "users");
+
+            migrationBuilder.DropTable(
+                name: "Drivers");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "users");
+
+            migrationBuilder.RenameColumn(
+                name: "RoleId",
+                table: "users",
+                newName: "role_id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_users_RoleId",
+                table: "users",
+                newName: "IX_users_role_id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ccp_number",
+                table: "users",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_users_roles_role_id",
+                table: "users",
+                column: "role_id",
+                principalTable: "roles",
+                principalColumn: "id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
