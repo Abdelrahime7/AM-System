729d50a122d874849f6d8cc984d9c8c2f32fc35a
Abdelrahime7 <abdououni67@gmail.com>
2025-10-22 09:30:08 +0100

    refact(DriverRepo):overrid getByid to include naviagation propertiy

--- src/Infrastructure/Repositories/DriverRepository.cs
+++ src/Infrastructure/Repositories/DriverRepository.cs
@@ -1,11 +1,16 @@
 ﻿using Application.Interfaces.Repositories;
 using Domain.Entities;
 using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories
 {
     public class DriverRepository(AppDbContext context)  :GenericRepository<Driver>(context),IDriverRepository
     {
-
+        public override async Task<Driver?> GetByIdAsync(int id)
+        {
+           return await  context.Drivers.Include(x=>x.User)
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
     }
 }
