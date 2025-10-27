cd00b9e23240c3615c58b1b1c42f07faf6424be8
Abdelrahime7 <abdououni67@gmail.com>
2025-10-27 10:44:13 +0100

    feat(application): add admin request validators
    Introduced CreatAdminValidators.cs to enforce validation rules for admin creation.
    
    Added UpdateAdminValidators.cs to validate update requests and ensure data integrity.

---
+++ src/Application/Admins/Validators/CreatAdminValidators.cs
@@ -0,0 +1,16 @@
+﻿using Application.Admins.Dto_s;
+using FluentValidation;
+
+
+namespace Application.Admins.Validators
+{
+    public class CreatAdminValidators :AbstractValidator<CreateAdminRequest>
+    {
+        public CreatAdminValidators()
+        {
+            RuleFor(x=>x.levels).IsInEnum().
+                NotEmpty().WithMessage("Admin Access Level Required");
+            
+        }
+    }
+}
---
+++ src/Application/Admins/Validators/UpdateAdminValidators.cs
@@ -0,0 +1,16 @@
+﻿using Application.Admins.Dto_s;
+using FluentValidation;
+
+namespace Application.Admins.Validators
+{
+    public class UpdateAdminValidators:AbstractValidator<UpdateAdminRequest>
+    {
+        public UpdateAdminValidators()
+        {
+            RuleFor(x => x.Id).NotEmpty().
+                GreaterThan(0).WithMessage("ID should be greater than 0");
+            RuleFor(x => x.levels).NotEmpty().
+                When(x => x.levels != null);
+        }
+    }
+}
