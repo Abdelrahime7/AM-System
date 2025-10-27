b6883bcd911c2a934609b04c99f0704ef44814fc
Abdelrahime7 <abdououni67@gmail.com>
2025-10-27 10:08:37 +0100

    feat(application): introduce admin and session DTOs
    Added AdminResponse, CreateAdminRequest, and UpdateAdminRequest for admin data exchange.
    
    Introduced session DTOs: AdminSessionResponse, CreateAdminSession, and UpdateAdminSession.
    
    These DTOs support structured request/response handling in admin workflows.

---
+++ src/Application/Admins/Dto's/session/AdminSessionResponse.cs
@@ -0,0 +1,12 @@
+﻿using Application.Admins.Dto_s;
+using Application.Users.DTOs;
+
+
+namespace Application.Drivers.DTO_s.session
+{
+    public class AdminSessionResponse
+    {
+        public UserResponse UserResponse { get; set; }
+        public AdminResponse AdminResponse {get; set; }
+    }
+}
