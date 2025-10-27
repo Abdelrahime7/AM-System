018a3b9ac1ca2aabd06dc88f7148b2e8fbdfb320
Abdelrahime7 <abdououni67@gmail.com>
2025-10-27 09:30:30 +0100

    feat(domain): add admin entity and access level enum
    Added Admin.cs to represent administrative users in the domain layer.
    
    Introduced AccessLevels.cs enum to define permission tiers for system roles.

---
+++ src/Domain/Entities/Admin.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Admin
+    {
+       public int Id { get; set; }
+        public  AccessLevels access { get ; set; }
+        public int UserID { get; set; }
+        public User user { get; set; }
+         
+    }
+}
