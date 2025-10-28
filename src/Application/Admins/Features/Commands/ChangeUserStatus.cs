7d165ed364a554ad03bfb7118a4ed08cf5df6d2f
Abdelrahime7 <abdououni67@gmail.com>
2025-10-28 11:04:54 +0100

    feat(application): implement ChangeUserStatus command with refined DTO
    Added ChangeUserStatus.cs to Admin command layer for toggling user status.
    
    Introduced ChangeStatusRequest.cs to encapsulate status change intent.
    
    Refactored changeUserStatus.cs to use ChangeStatusRequest instead of full UpdateUserRequest.
    
    Updated IAdminCommands and IUserCommands interfaces to support new command signature.

---
+++ src/Application/Admins/Features/Commands/ChangeUserStatus.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Models;
+using Application.Users.DTOs;
+
+namespace Application.Admins.Features.Commands
+{
+    partial class AdminCommands
+    {
+        public  async Task<Result<bool>> ChangeUserStatusAsync(ChangeStatusRequest request)
+        {
+            return await _userCommands.ChangeUserStatusAsync(request);
+        }
+    }
+}
--- src/Application/Interfaces/AdminInterfaces/IAdminCommands.cs
+++ src/Application/Interfaces/AdminInterfaces/IAdminCommands.cs
@@ -1,7 +1,9 @@
 ﻿
 
-using Application.Common.Models;
 using Application.Admins.DTO_s.session;
+using Application.Common.Models;
+using Application.Users.DTOs;
+using Domain.Enums;
 
 namespace Application.Interfaces.AdminInterfaces
 {
@@ -10,6 +12,9 @@ namespace Application.Interfaces.AdminInterfaces
         public Task<Result<int>> CreateAdminAsync(CreatAdminSession request);
         public Task<Result<bool>> DeleteAdminAsnc(int Id);
         public Task<Result<bool>> UpdateAdminAsnc(UpdateAdminSession request);
+
+        public Task<Result<bool>> ChangeUserStatusAsync(ChangeStatusRequest request);
+
 
     }
 }
