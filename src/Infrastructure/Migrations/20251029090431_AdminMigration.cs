0fbda77b2d7b17827d81c0fc3c4a7453e0c3469e
Abdelrahime7 <abdououni67@gmail.com>
2025-10-29 10:16:10 +0100

    migration(infrastructure): scaffold AdminMigration for Admins table
    Added AdminMigration.cs and its designer file to define schema changes for Admins entity.
    
    Updated AppDbContextModelSnapshot to reflect the new Admins table structure.

---
+++ src/Infrastructure/Migrations/20251029090431_AdminMigration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdminMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    AccessLevel = table.Column<int>(type: "integer", nullable: false),
+                    UserID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Admins_users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Admins_UserID",
+                table: "Admins",
+                column: "UserID",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+        }
+    }
+}
