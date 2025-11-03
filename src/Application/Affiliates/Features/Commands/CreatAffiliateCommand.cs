ec028806388425065ebe15536cc807e82c12b8b4
Abdelrahime7 <abdououni67@gmail.com>
2025-11-03 12:04:38 +0100

    feat(affiliate): implement commands and queries for feature layer
    Introduced Create/UpdateAffiliateCommand for write operations
    
    Added GetAffiliateByIdQuery and GetAllAffiliatesQuery for read operations
    
    Implemented IAffiliatCommands and IAffiliateQueries interfaces for CQRS abstraction

---
+++ src/Application/Affiliates/Features/Commands/CreatAffiliateCommand.cs
@@ -0,0 +1,46 @@
+﻿using Application.Affiliates.DTO_s;
+using Application.Affiliates.DTO_s.session;
+using Application.Common.Models;
+using Application.Interfaces.AffiliateInterfaces;
+using Application.Interfaces.Common.Mappers;
+using Application.Interfaces.Repositories;
+using Application.Interfaces.UserInterfaces;
+using Domain.Entities;
+
+namespace Application.Affiliates.Features.Commands
+{
+    partial class AffiliateCommands( IAffiliateRepository repository,
+           IUserCommands  commands,
+           IEntityMapper<Affiliate, CreateAffiliateRequest, UpdateAffiliateRequest,
+           AffiliateResponse> mapper ) : IAffiliatCommands
+    {
+        private readonly IUserCommands _userCommands = commands;
+        private readonly IAffiliateRepository _repository = repository;
+        private readonly IEntityMapper<Affiliate, CreateAffiliateRequest,
+            UpdateAffiliateRequest,   AffiliateResponse> _mapper=mapper;
+
+        public async Task<Result<int>> CreateAffiliateAsync(CreatAffiliateSession request)
+        {
+            try
+            {
+                var User = await _userCommands.CreatUserAsync(request.UserRequest);
+
+                var Affiliate = _mapper.ToEntity(request.AffiliateRequest);
+
+                Affiliate.user = User.Value;
+                await _repository.AddAsync(Affiliate);
+
+                return Result<int>.Success(Affiliate.Id);
+
+            }
+            catch (Exception ex)
+            {
+                return Result<int>.Failure("Failed to add Affiliate");
+            }
+        }
+
+      
+
+      
+    }
+}
---
+++ src/Application/Affiliates/Features/Commands/UpdateAffiliateCommand.cs
@@ -0,0 +1,34 @@
+﻿using Application.Affiliates.DTO_s.session;
+using Application.Common.Models;
+
+
+namespace Application.Affiliates.Features.Commands
+{
+    partial class AffiliateCommands
+    {
+
+        public async Task<Result<bool>> UpdateAffiliateAsnc(UpdateAffiliateSession request)
+        {
+            try
+            {
+                var Affiliate = await _repository.GetByIdAsync(request.AffiliateRequest.Id);
+
+                if (Affiliate == null)
+                    return Result<bool>.Failure("Affiliate not found");
+
+                if (request.UserRequest != null)
+                {
+                    await _userCommands.UpdateUserAsync(request.UserRequest);
+                }
+                _mapper.ToUpdateEntity(Affiliate, request.AffiliateRequest);
+
+                _repository.Update(Affiliate);
+                return Result<bool>.Success(true);
+            }
+            catch (Exception e)
+            {
+                return Result<bool>.Failure($"Failed to update Affiliate: {e.Message}");
+            }
+        }
+    }
+}
