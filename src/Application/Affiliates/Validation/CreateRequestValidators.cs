2a70cb092ddd2f48b73378b740ad2c70daf24b87
Abdelrahime7 <abdououni67@gmail.com>
2025-11-03 11:58:14 +0100

    feat(affiliate): add validators for create/update requests
    Implemented FluentValidation rules for CreateAffiliateRequest and UpdateAffiliateRequest
    
    Ensures input integrity and enforces business constraints at the application boundary

---
+++ src/Application/Affiliates/Validation/CreateRequestValidators.cs
@@ -0,0 +1,23 @@
+﻿using Application.Affiliates.DTO_s;
+using FluentValidation;
+
+namespace Application.Affiliates.Validation
+{
+    public class CreateRequestValidators:AbstractValidator<CreateAffiliateRequest>
+    {
+        public CreateRequestValidators()
+        {
+            RuleFor(x=>x.ReferralCode).NotEmpty().
+                GreaterThan(0).WithMessage("Referral Code required");
+
+            RuleFor(x => x.CommissionRate).NotEmpty().When(x => x!=null).
+                WithMessage("Commission Rate required");
+
+            RuleFor(x => x.PartnerSince).NotEmpty().When(x => x != null)
+                .WithMessage("Partner Since date is required ");
+
+
+        }
+
+    }
+}
