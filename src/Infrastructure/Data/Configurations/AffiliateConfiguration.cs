3d752851aec1c1c4b9a7bc6edf1138f0e9c932b8
Abdelrahime7 <abdououni67@gmail.com>
2025-11-03 10:02:13 +0100

    feat(affiliate): scaffold repository, entity, and configuration
    Added IAffiliateRepository interface for domain abstraction
    
    Defined Affiliate entity with relevant properties
    
    Registered Affiliate in AppDbContext and configured mapping via AffiliateConfiguration
    
    Implemented AffiliateRepository with EF Core integration

---
+++ src/Infrastructure/Data/Configurations/AffiliateConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Infrastructure.Data.Configurations
+{
+    public class AffiliateConfiguration : IEntityTypeConfiguration<Affiliate>
+    {
+        public void Configure(EntityTypeBuilder<Affiliate> builder)
+        {
+
+            builder.ToTable("Affiliates");
+
+            builder.HasKey(x => x.Id);
+
+            builder. Property(d => d.Id)
+                .HasColumnName("id")
+                .ValueGeneratedOnAdd(); ;
+
+            builder.Property(x => x.UserID).
+                HasColumnName("User_id")
+                .IsRequired();
+
+            builder.Property(x => x.ReferralCode).
+                HasColumnName("ReferralCode");
+
+            builder.Property(x => x.CommissionRate).
+               HasColumnName("Commission_Rate");
+
+
+            builder.Property(x => x.PartnerSince).
+               HasColumnName("Partner_Since");
+
+
+            builder.HasOne(x=>x.user).
+                WithOne().HasForeignKey<Affiliate>(x=>x.UserID)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+
+        }
+    }
+}
---
+++ src/Infrastructure/Repositories/AffiliateRepository.cs
@@ -0,0 +1,10 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using Infrastructure.Data;
+namespace Infrastructure.Repositories
+{
+    public class AffiliateRepository(AppDbContext context) :GenericRepository<Affiliate>(context) ,IAffiliateRepository
+    {
+       
+    }
+}
