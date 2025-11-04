0a2e9c2fabdc9b109c45d3f73aaeaa7cfac83e44
Abdelrahime7 <abdououni67@gmail.com>
2025-11-04 10:11:48 +0100

    ovveride getbyid method to fit eager loading

--- src/Infrastructure/Repositories/AffiliateRepository.cs
+++ src/Infrastructure/Repositories/AffiliateRepository.cs
@@ -1,10 +1,15 @@
 ﻿using Application.Interfaces.Repositories;
 using Domain.Entities;
 using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 namespace Infrastructure.Repositories
 {
     public class AffiliateRepository(AppDbContext context) :GenericRepository<Affiliate>(context) ,IAffiliateRepository
     {
-       
+        public override async Task<Affiliate?> GetByIdAsync(int id)
+        {
+          return    await context.Affiliates.Include(x => x.user)
+                 .FirstOrDefaultAsync(x => x.Id == id);
+        }
     }
 }
