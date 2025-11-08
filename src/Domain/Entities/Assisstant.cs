8cfe20620e773257e8c021cf7df09d92813c571e
Abdelrahime7 <abdououni67@gmail.com>
2025-11-08 09:32:14 +0100

    feat(domain): add Assisstant entity and EF Core configuration
    Defined Assisstant.cs with relevant properties for domain modeling
    
    Registered Assisstant in AppDbContext and applied configuration via AssisstantConfiguration

---
+++ src/Domain/Entities/Assisstant.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Domain.Entities
+{
+    public class Assisstant
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int AssignedBy { get; set; }   
+        public User User { get; set; }
+       
+    }
+}
--- src/Infrastructure/Data/AppDbContext.cs
+++ src/Infrastructure/Data/AppDbContext.cs
@@ -8,6 +8,7 @@ namespace Infrastructure.Data;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
+    public DbSet<Assisstant> Assisstants { get; set; }
     public DbSet<Affiliate> Affiliates { get; set; }
     public DbSet<Admin> Admins { get; set; }
     public DbSet<User> Users { get; set; }
