b3aee88bbe309f61f15e5bbde7a3b101376a191c
Abdelrahime7 <abdououni67@gmail.com>
2026-01-10 13:42:25 +0100

    feat(auth): add login request DTO and auth controller

---
+++ src/WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using Application.Users.CredentialChecker;c
+using Application.Users.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly TokenService _tokenService;
+        private readonly ICredentialChecker _credentialChecker;
+
+        public AuthController(TokenService tokenService, ICredentialChecker credentialChecker)
+        {
+            _tokenService = tokenService;
+            _credentialChecker = credentialChecker;
+        }
+
+        [HttpPost("login")]
+
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Username) || string.IsNullOrWhiteSpace(request.Password))
+                return BadRequest("Username and password are required.");
+
+            var identity = await _credentialChecker.CheckCredentialsAsync(request.Username, request.Password);
+            if (identity == null)
+                return Unauthorized("Invalid username or password.");
+
+            var claims = new List<Claim>
+    {
+        new Claim(JwtRegisteredClaimNames.Sub, identity.id.ToString()),
+        new Claim(ClaimTypes.Role, identity.Role.ToString())
+    };
+
+            var accessToken = _tokenService.GenerateAccessToken(claims);
+            var refreshToken = _tokenService.GenerateRefreshToken();
+
+            // todo :save refreshToken in DB with userId
+          
+
+            return Ok(new { accessToken, refreshToken });
+        }
+
+
+    }
+
+
+}
