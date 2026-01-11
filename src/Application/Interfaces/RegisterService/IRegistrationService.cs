92006b97667d2a473152c6c39c6df5a61b55c5bf
Abdelrahime7 <abdououni67@gmail.com>
2026-01-11 14:17:10 +0100

    feat(auth): introduce RegistrationService and role request DTOs

---
+++ src/Application/Interfaces/RegisterService/IRegistrationService.cs
@@ -0,0 +1,12 @@
+﻿
+
+using Application.Common.Models;
+using Application.RoleRequeste;
+
+namespace Application.Interfaces.RegisterService
+{
+    public interface IRegistrationService
+    {
+        Task<Result<int>> RegisterAsync(CreateRoleSession request);
+    }
+}
