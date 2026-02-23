258a0705f38042cdcf74b16df7b427f1924a2f25
Abdelrahime7 <abdououni67@gmail.com>
2026-02-23 11:19:21 +0100

    refactor(application): update repository interfaces
    - Adjusted csproj references
    - Refined Affiliate and Order repository contracts

--- src/Application/Interfaces/Repositories/IAffiliateRepository.cs
+++ src/Application/Interfaces/Repositories/IAffiliateRepository.cs
@@ -4,6 +4,6 @@ namespace Application.Interfaces.Repositories
 {
     public interface IAffiliateRepository:IGenericRepository<Affiliate>
     {
-
+        Task<int> CountActiveAsync(CancellationToken cancellationToken= default);
     }
 }
--- src/Application/Interfaces/Repositories/IOrderRepository .cs
+++ src/Application/Interfaces/Repositories/IOrderRepository .cs
@@ -7,5 +7,8 @@ public interface IOrderRepository : IGenericRepository<Order>
 {
 
     Task CommitAsync(CancellationToken cancellationToken = default);
+    Task<int> CountPendingAsync(CancellationToken cancellationToken = default);
+
+Task <decimal> TotalSalesAsync(CancellationToken cancellationToken = default); 
 
 }
