d07b4e95881e010d01603a1c9cb8b3d0af42a9e8
Abdelrahime7 <abdououni67@gmail.com>
2026-02-23 11:15:57 +0100

    feat(webapi): add GraphQL support
    - Integrated GraphQL endpoint structure
    - Updated Program.cs for middleware registration
    - Adjusted WebAPI project references

---
+++ src/WebAPI/Graphql/Query.cs
@@ -0,0 +1,18 @@
+﻿using Application.Admins.Dasboard.DashDto;
+using Application.Admins.Dasboard.DashMetrics.Application.Admin.Dashboard;
+using HotChocolate.Authorization;
+
+
+namespace WebAPI.Graphql
+{
+    public class Query
+    {
+        [Authorize(Policy = "SuperAdminOnly")]
+
+        public async Task<DashboardDto> GetDashboard([Service] GetDashboardMetrics useCase)
+        {
+            return await useCase.Execute();
+        }
+    }
+
+}
