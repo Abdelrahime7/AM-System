f6a3f21451dc3b845fc9b6b97074a806c9f0cfda
Abdelrahime7 <abdououni67@gmail.com>
2026-03-03 11:43:51 +0100

    feat(app): integrate token refresh into GraphQL client and services

--- frontend/amsfront/lib/features/SuperAdmin/data/services/dashboard_service.dart
+++ frontend/amsfront/lib/features/SuperAdmin/data/services/dashboard_service.dart
@@ -2,25 +2,7 @@ import 'package:amsfront/features/SuperAdmin/data/model/dashboardModel.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 
-class DashboardService {
-  final GraphQLClient client;
 
-  DashboardService(this.client);
-
-  Future<DashboardModel> getDashboardData() async {
-    final QueryOptions options = QueryOptions(
-      document: gql(dashboardQuery),
-    );
-
-    final QueryResult result = await client.query(options);
-
-    if (result.hasException) {
-      throw Exception(result.exception.toString());
-    }
-return DashboardModel.fromJson(result.data?['dashboard']);
-
-  }
-}
 
 
 
@@ -34,3 +16,26 @@ const String dashboardQuery = r'''
     }
   }
 ''';
+
+
+class DashboardService {
+  final GraphQLClient client;
+
+  DashboardService(this.client);
+
+
+  Future<DashboardModel> getDashboardData() async {
+    final QueryOptions options = QueryOptions(
+      document: gql(dashboardQuery),
+    );
+
+
+    final result = await client.query(options);
+
+    if (result.hasException) {
+      throw Exception(result.exception.toString());
+    }
+
+    return DashboardModel.fromJson(result.data?['dashboard']);
+  }
+}
